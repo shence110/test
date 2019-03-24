edb6b5b4f02636ab8311e94e684c8a06dd62cadf
a123yaoyao <291778952@qq.com>
2019-03-24 15:00:02 +0800

    多线程插入数据

--- src/main/java/com/neo/Application.java
+++ src/main/java/com/neo/Application.java
@@ -4,9 +4,11 @@ import com.neo.util.SpringContextUtil;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
